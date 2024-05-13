382f1b2b181b9eb56e1aeae6da1fd4c5bd5b1d0a
mokus <maxym.hryniv@gmail.com>
2024-05-13 11:28:14 +0300

    App: beep sound moved away from the speech_to_text engine and changed

--- speech_to_text/ios/Classes/SwiftSpeechToTextPlugin.swift
+++ speech_to_text/ios/Classes/SwiftSpeechToTextPlugin.swift
@@ -370,7 +370,7 @@ public class SwiftSpeechToTextPlugin: NSObject, FlutterPlugin {
             }
             rememberedAudioCategory = self.audioSession.category
             rememberedAudioCategoryOptions = self.audioSession.categoryOptions
-            try self.audioSession.setCategory(AVAudioSession.Category.playAndRecord, options: [.allowBluetoothA2DP,.mixWithOthers])
+            try self.audioSession.setCategory(AVAudioSession.Category.playAndRecord, options: [.allowBluetoothA2DP,.mixWithOthers,.defaultToSpeaker])
             //            try self.audioSession.setMode(AVAudioSession.Mode.measurement)
             if ( sampleRate > 0 ) {
                 try self.audioSession.setPreferredSampleRate(Double(sampleRate))
