4ae83b4c2b71709385f43cf9f1f10cfd142df29a
mokus <maxym.hryniv@gmail.com>
2026-07-09 20:07:04 +0300

    App: allowing bluetooth speakers

--- speech_to_text/ios/Classes/SwiftSpeechToTextPlugin.swift
+++ speech_to_text/ios/Classes/SwiftSpeechToTextPlugin.swift
@@ -344,7 +344,7 @@ public class SwiftSpeechToTextPlugin: NSObject, FlutterPlugin {
 
             rememberedAudioCategory = self.audioSession.category
             rememberedAudioCategoryOptions = self.audioSession.categoryOptions
-            try self.audioSession.setCategory(AVAudioSession.Category.playAndRecord, options: [.allowBluetooth,.defaultToSpeaker])
+            try self.audioSession.setCategory(AVAudioSession.Category.playAndRecord, options: [.allowBluetooth,.allowBluetoothA2DP,.defaultToSpeaker])
             //            try self.audioSession.setMode(AVAudioSession.Mode.measurement)
             //        if ( sampleRate > 0 ) {
             //            try self.audioSession.setPreferredSampleRate(Double(sampleRate))
